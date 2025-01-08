f90b388f3c78a849fc8cafe5254abe8781a183a9
sinthuyan27 <nanthakumarsinthuyan13@gmail.com>
2025-01-08 15:49:29 +0530

    routerAdded

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,7 +1,7 @@
 import React from "react"; //6.9k (gzipped:2.7k)
-import Header from "../header/header";  
-import Routers from "../router/router";
-import Footer from "../footer/footer";
+import Header from "../header";  
+import Routers from "../../router/router";
+import Footer from "../../components/footer/footer";
 
 const Layout = () => {
     return (
